c564866bd970ede15076ea3dd549b4f25323dcd9
reindert <info@rjekker.nl>
2015-11-10 20:23:24 +0100

    Rit Entity added

---
+++ src/nl/zwolle/model/Rit.java
@@ -0,0 +1,59 @@
+package nl.zwolle.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Rit {
+
+	private Long id;
+	private String omschrijving;
+	private BigDecimal start;
+	private BigDecimal end;
+	
+
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	public Long getId() {
+	    return id;
+	}
+	
+	
+	public Rit(){}
+	
+	public void setId(Long id){
+		this.id = id;
+	}
+	
+	
+	public String getOmschrijving() {
+		return omschrijving;
+	}
+
+	public void setOmschrijving(String omschrijving) {
+		this.omschrijving = omschrijving;
+	}
+
+	public BigDecimal getStart() {
+		return start;
+	}
+
+	public void setStart(BigDecimal start) {
+		this.start = start;
+	}
+
+	public BigDecimal getEnd() {
+		return end;
+	}
+
+	public void setEnd(BigDecimal end) {
+		this.end = end;
+	}
+	
+}
